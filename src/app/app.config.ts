03beea70ef1c07fe1454da67be6400329e4337ed
Fraigneau Lucas <fraigneaulucas@proton.me>
2026-07-15 17:06:28 +0200

    feat: add PageNotFound component and update routing for 404 handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfile } from './pokemon/pokemon-profile/pokemon-profile';
+import { PageNotFound } from './config/page-not-found/page-not-found';
 
 const routes: Routes = [
   { path: 'pokemons/:id', component: PokemonProfile },
   { path: 'pokemons', component: PokemonListComponent },
-  {path: '', redirectTo: 'pokemons', pathMatch: 'full' },
+  { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
+  { path: '**', component: PageNotFound },
 ];
 
 export const appConfig: ApplicationConfig = {
