964ab57f8f797136bd6bf724ebbaf103af939f29
Fraigneau Lucas <fraigneaulucas@proton.me>
2026-07-15 17:53:28 +0200

    feat: add edit route for Pokémon and update profile component with edit link

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,10 @@ import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfile } from './pokemon/pokemon-profile/pokemon-profile';
 import { PageNotFound } from './config/page-not-found/page-not-found';
+import { PokemonEdit } from './pokemon/pokemon-edit/pokemon-edit';
 
 const routes: Routes = [
+  { path: 'pokemons/edit/:id', component: PokemonEdit, title: 'Edit Pokémon' },
   { path: 'pokemons/:id', component: PokemonProfile, title: 'Pokémon' },
   { path: 'pokemons', component: PokemonListComponent, title: 'Pokédex' },
   { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
