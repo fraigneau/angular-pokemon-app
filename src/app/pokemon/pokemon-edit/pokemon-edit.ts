00c2f21d378cf4d102204c5d389084fe7c67431c
Fraigneau Lucas <fraigneaulucas@proton.me>
2026-07-17 02:23:49 +0200

    feat: enhance Pokémon edit form with reactive controls and type management

--- src/app/pokemon/pokemon-edit/pokemon-edit.ts
+++ src/app/pokemon/pokemon-edit/pokemon-edit.ts
@@ -1,13 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../service/pokemonService';
-import {
-  FormArray,
-  FormControl,
-  FormGroup,
-  isFormControl,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pokemon-edit',
@@ -27,4 +21,29 @@ export class PokemonEdit {
     damage: new FormControl(this.pokemon().damage),
     type: new FormArray(this.pokemon().types.map((type) => new FormControl(type))),
   });
+
+  get pokemonTypesList(): FormArray {
+    return this.form.get('type') as FormArray;
+  }
+
+  isPokemonTypesSelected(type: String): boolean {
+    return !!this.pokemonTypesList.controls.find((control) => control.value === type);
+  }
+
+  onPokemonTypeChange(type: String, isChecked: boolean) {
+    if (isChecked) {
+      const control = new FormControl(type);
+      this.pokemonTypesList.push(control);
+    } else {
+      const index = this.pokemonTypesList.controls
+        .map((control) => control.value)
+        .indexOf(type);
+
+      this.pokemonTypesList.removeAt(index);
+    }
+  }
+
+  onSubmit(){
+    console.log(this.form.value);
+  }
 }
