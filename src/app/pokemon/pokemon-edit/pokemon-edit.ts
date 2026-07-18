36d035b986d48f7d444d4509deda1d666f759e10
Fraigneau Lucas <fraigneaulucas@proton.me>
2026-07-18 19:14:48 +0200

    feat: enhance Pokémon edit form with validation rules for name, life, damage, and type

--- src/app/pokemon/pokemon-edit/pokemon-edit.ts
+++ src/app/pokemon/pokemon-edit/pokemon-edit.ts
@@ -1,9 +1,9 @@
 import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../service/pokemonService';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { getPokemonColor} from '../../model/pokemon.model';
-import { CommonModule} from '@angular/common';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { getPokemonColor, POKEMON_RULES } from '../../model/pokemon.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-edit',
@@ -19,10 +19,27 @@ export class PokemonEdit {
   protected readonly getPokemonColor = getPokemonColor;
 
   readonly form = new FormGroup({
-    name: new FormControl(this.pokemon().name),
-    life: new FormControl(this.pokemon().life),
-    damage: new FormControl(this.pokemon().damage),
-    type: new FormArray(this.pokemon().types.map((type) => new FormControl(type))),
+    name: new FormControl(this.pokemon().name, [
+      Validators.required,
+      Validators.pattern(POKEMON_RULES.NAME_PATTERN),
+      Validators.maxLength(POKEMON_RULES.MAX_NAME),
+      Validators.minLength(POKEMON_RULES.MIN_NAME),
+    ]),
+    life: new FormControl(this.pokemon().life, [
+      Validators.required,
+      Validators.max(POKEMON_RULES.MAX_LIFE),
+      Validators.min(POKEMON_RULES.MIN_LIFE),
+    ]),
+    damage: new FormControl(this.pokemon().damage, [
+      Validators.required,
+      Validators.max(POKEMON_RULES.MAX_DAMAGE),
+      Validators.min(POKEMON_RULES.MIN_DAMAGE),
+    ]),
+    type: new FormArray(this.pokemon().types.map((type) => new FormControl(type)), [
+      Validators.required,
+      Validators.maxLength(POKEMON_RULES.MAX_TYPES),
+      Validators.minLength(POKEMON_RULES.MIN_TYPES),
+    ]),
   });
 
   get pokemonTypesList(): FormArray {
@@ -46,6 +63,12 @@ export class PokemonEdit {
 
   protected getTextColor(type: string): string {
     return type === 'Electrik' ? '#000' : '#FFF';
+  }
+
+  protected POKEMON_RULES = POKEMON_RULES;
+
+  get pokemonName(): FormControl {
+    return this.form.get('name') as FormControl;
   }
 
   onSubmit() {
