4a8d5e7812649ec47f3f1e38f3dcc5ed6b6b2268
Fraigneau Lucas <fraigneaulucas@proton.me>
2026-07-20 19:06:31 +0200

    feat: enhance Pokémon profile component with loading and error handling states

--- src/app/pokemon/pokemon-profile/pokemon-profile.ts
+++ src/app/pokemon/pokemon-profile/pokemon-profile.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../service/pokemonService';
 import { DatePipe } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-profile',
@@ -13,8 +14,15 @@ import { toSignal } from '@angular/core/rxjs-interop';
 export class PokemonProfile {
   readonly #route = inject(ActivatedRoute);
   readonly #pokemonService = inject(PokemonService);
-
   readonly #pokemonId = Number(this.#route.snapshot.paramMap.get('id'));
 
-  readonly pokemon = toSignal(this.#pokemonService.getPokemonById(this.#pokemonId));
+  readonly #pokemonResponse = toSignal(
+    this.#pokemonService.getPokemonById(this.#pokemonId).pipe(
+      map(pokemon => ({value: pokemon, error: undefined})),
+      catchError(error => of({value: undefined, error: error}))
+  ))
+
+  readonly loading = computed(() => this.#pokemonResponse === undefined);
+  readonly error = computed(() => this.#pokemonResponse()?.error !== undefined);
+  readonly pokemon = computed(() => this.#pokemonResponse()?.value);
 }
